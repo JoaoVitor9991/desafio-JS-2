ad3c60359464f1f3f6aa010066fb6cde694b47d5
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-03 15:13:58 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert("oi")
+
+console.log("OIIIIIIIII");
+
+
+//let i = 0;
+
+
+//while (i <= 500) {
+  
+  
+//  document.write("NOVO TEXTO <BR>" );
+  
+  
+ // i++; 
+//}
+
+for( var x = 0 ; x <= 100; x ++){
+    document.write(x + "<br>");
+}
