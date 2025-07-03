fcb134af90c6c15c1694a03ac8562d2fb49efee0
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-03 15:50:09 -0300

    ATT

--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,4 @@
-let lista = ['Maça', 'Banana', 'Mamão', 'Laranja', 'Uva', 'Monster', 'Limão', 'Pinga', 'Detergente', 'Essência']
+let lista = ['Maça', 'Banana', 'Mamão', 'Laranja', 'Uva', 'Monster', 'Limão', 'Pinga', 'Carvão', 'Essência']
 
 console.log(lista.length)
 
@@ -8,15 +8,18 @@ let nome = "João Vitor Machado Oliveira"
 
 //console.log("Qntd de caracteres? " +nome.length)
 
-if(nome.length > 25){
-    console.log("nome grande")
-} else {
-    console.log("Nome pequeno")
-}
 
 console.log (lista[0])
 
+let conteudo = '';
+let div = document.getElementById("textos");
+
+
+
 for (let i = 0; i < lista.length ; i++){
-   // console.log (lista [i] );
-   document.write('<h1>' + lista[i] + '</h1>');
+    conteudo += '<h1>'+lista[i]+'</h1> <br>';
 }
+
+div.innerHTML = conteudo;
+
+console.log(conteudo);
