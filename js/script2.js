fbb0e3b92809012200f006aa1d4ad6e3821b1ef8
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-03 15:18:10 -0300

    att

---
+++ js/script2.js
@@ -0,0 +1,7 @@
+let lista = ['Maça', 'Banana', 'Mamão', 'Laranja', 'Uva', 'Monster', 'Limão', 'Pinga', 'Detergente', 'Essência']
+
+console.log(lista.length)
+
+let nome = "João Vitor Machado Oliveira"
+
+console.log("Qntd de caracteres? " +nome.length)
