42500c983ef4dc91724fae8179066ae926152a0e
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-03 15:20:26 -0300

    att

--- js/script2.js
+++ js/script2.js
@@ -2,6 +2,14 @@ let lista = ['Maça', 'Banana', 'Mamão', 'Laranja', 'Uva', 'Monster', 'Limão',
 
 console.log(lista.length)
 
-let nome = "João Vitor Machado Oliveira"
+let nome = prompt("Digite seu nome");
+
+//let nome = "João Vitor Machado Oliveira"
 
 console.log("Qntd de caracteres? " +nome.length)
+
+if(nome.length > 25){
+    console.log("nome grande")
+} else {
+    console.log("Nome pequeno")
+}
